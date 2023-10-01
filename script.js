071f6011ae94e32279b8d7bb8ed830fa59abe90b
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-10-01 23:42:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstChar(text) {
+	for(let i = 0; i<text.length; i++){
+		if(text[i] != ' '){
+			return text[i];
+		}
+	}
   // your code here
+	return '';
 }
 
 // Do not change the code below
 
 const text = prompt("Enter text:");
+
 alert(firstChar(text));
